8b4ebb270766d452aab4f14a9a3cc912af146a4f
Elijah991 <evansampaire@.com>
2024-04-23 02:59:19 +0300

    rank

---
+++ if_Statement.py
@@ -0,0 +1,10 @@
+# Define a number
+number = 7
+
+# Use an if statement to check if the number is even or odd
+if number % 2 == 0:
+    # If the number is even, print "Even"
+    print("Even")
+else:
+    # If the number is odd, print "Odd"
+    print("Odd")
---
+++ loop.py
@@ -0,0 +1,7 @@
+# Define a list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# Use a for loop to iterate over the list
+for number in numbers:
+    # Print each number
+    print(number)
