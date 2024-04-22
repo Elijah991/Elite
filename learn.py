7deee6640a4d5b70482504a134ed5212d9248edc
Elijah991 <evansampaire@.com>
2024-04-23 02:54:56 +0300

    ree

--- learn.py
+++ learn.py
@@ -3,10 +3,10 @@ Age = int(input("How old are you:"))
 
 print(Name, "is", Age, "years old")
 
-# Function to calculate the area of a rectangle
-def calculate_area(length, width):
-    area = length * width
-    return area
+# # Function to calculate the area of a rectangle
+# def calculate_area(length, width):
+#     area = length * width
+#     return area
 
 # User input for length and width
 length = float(input("Enter the length of the rectangle: "))
@@ -15,3 +15,13 @@ width = float(input("Enter the width of the rectangle: "))
 # Calculate and display the area
 area = calculate_area(length, width)
 print("The area of the rectangle is:", area)
+
+class MyClass:
+    def my_method(self):
+        print("Hello, World!")
+
+# Create an instance of MyClass
+my_instance = MyClass()
+
+# Call the my_method of my_instance
+my_instance.my_method()
