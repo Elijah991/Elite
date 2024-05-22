8102fa43d3b9bb09b11cc11fd78165682f1afcb5
Elijah991 <evansampaire@.com>
2024-05-22 22:38:56 +0300

    one

---
+++ lists.py
@@ -0,0 +1,12 @@
+my_list = [1, 10, 3, 10, 5]
+my_list.remove(10)
+print(my_list)  # This will print [1, 3, 10, 5]
+
+# my_list = [1, 10, 3, 10, 5]
+# removed_value = my_list.pop(2)
+# print(my_list)  # This will print [1, 10, 10, 5]
+# print(removed_value)  # This will print 3
+
+my_list = [1, 10, 3, 10, 5]
+my_list.clear()
+print(my_list)  # This will print []
